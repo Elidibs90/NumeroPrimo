9231b7a04c0709fcaa7e996cd6f010f833703b0b
Andres Castillo <elidibs90@gmail.com>
2025-01-31 17:17:19 -0500

    add business and facade

--- src/bussiness/NumeroPrimo.java
+++ src/bussiness/NumeroPrimo.java
@@ -1,4 +1,18 @@
 package bussiness;
 
 public class NumeroPrimo {
+    public boolean checkIsPrimeNumber(int n) {
+
+        if(n<=1){
+            return false;
+        }
+
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/facade/UserUI.java
+++ src/facade/UserUI.java
@@ -1,4 +1,26 @@
 package facade;
 
+import javax.swing.*;
+
 public class UserUI {
+
+    public void numberIn() {
+        boolean menu = false;
+        while(menu!=true) {
+            String typeYourPrimeNumber = JOptionPane.showInputDialog("Ingresa un numero primo");
+            try {
+                Integer.parseInt(typeYourPrimeNumber);
+                String mensaje= "El numero "+typeYourPrimeNumber+" es un numero primo";
+                JOptionPane.showMessageDialog(null,mensaje);
+                menu = true;
+            } catch (NumberFormatException e) {
+                JOptionPane.showInputDialog("Ingresa un numero valido");
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+
+
 }
