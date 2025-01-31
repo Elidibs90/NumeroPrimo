2feab6a498961b2cc01ab10c2f31fe05bef8e3c2
Andres Castillo <elidibs90@gmail.com>
2025-01-31 17:45:10 -0500

    Add readme, fix the logic and add the main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import facade.UserUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        UserUI ui = new UserUI();
+        ui.numberIn();
+
     }
 }
--- src/facade/UserUI.java
+++ src/facade/UserUI.java
@@ -1,21 +1,29 @@
 package facade;
 
+import bussiness.NumeroPrimo;
+
 import javax.swing.*;
 
 public class UserUI {
 
     public void numberIn() {
         boolean menu = false;
+        int number = 0;
         while(menu!=true) {
             String typeYourPrimeNumber = JOptionPane.showInputDialog("Ingresa un numero primo");
             try {
-                Integer.parseInt(typeYourPrimeNumber);
-                String mensaje= "El numero "+typeYourPrimeNumber+" es un numero primo";
-                JOptionPane.showMessageDialog(null,mensaje);
-                menu = true;
+                number =Integer.parseInt(typeYourPrimeNumber);
+                NumeroPrimo np = new NumeroPrimo();
+                if (np.checkIsPrimeNumber(number)) {
+                    String mensaje= "El numero "+typeYourPrimeNumber+" es un numero primo";
+                    JOptionPane.showMessageDialog(null,mensaje);
+                    menu = true;
+                }
+
+
             } catch (NumberFormatException e) {
                 JOptionPane.showInputDialog("Ingresa un numero valido");
-                throw new RuntimeException(e);
+                
             }
 
         }
